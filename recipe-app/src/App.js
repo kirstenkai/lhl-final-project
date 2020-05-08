fd657ec81b35ab50f8e210eed848ec60dda083b9
BilliMorgan <shimko20@gmail.com>
2020-05-08 17:57:21 +0000

    need to cancel all changes

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">Hello World</div>
     <div className="App">
-      <Button variant="success">Success</Button>{" "}
+     
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
