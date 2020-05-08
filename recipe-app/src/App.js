15c3746e7da45aec0db922df50bb26884e14975f
Jeffrey <jeffrey.liu90@gmail.com>
2020-05-07 22:49:45 -0400

    Connected React with Express backend

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  Axios({
+    method: "GET",
+    url: "http://localhost:5000/",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data.message);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
