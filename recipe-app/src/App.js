30506918ec4e0c01dd36d3a09ffe45f096083f18
Jeffrey <jeffrey.liu90@gmail.com>
2020-05-08 17:47:20 -0400

    Fixed up component for Header and Registration

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Axios from "axios";
+import Header from "./components/Header";
+import Registration from "./components/Registration";
 
 function App() {
   Axios({
@@ -16,20 +18,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Registration />
     </div>
   );
 }
--- recipe-app/src/components/Registration.js
+++ recipe-app/src/components/Registration.js
@@ -1,69 +1,106 @@
 import React from "react";
 
 const Registration = () => {
+  const register = e => {
+    e.preventDefault();
+    console.log("register");
+  };
+
   return (
     <div>
-      <form className="registration" method="POST" action="/">
-        <div class="container">
-          <div>
-            <label for="first-name">
-              <b>First Name</b>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter First Name"
-              name="name"
-              required
-            />
-          </div>
-
-          <div>
-            <label for="last-name">
-              <b>Last Name</b>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Last Name"
-              name="name"
-              required
-            />
-          </div>
-
-          <div>
-            <label for="e-mail">
-              <b>E-mail</b>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Last Name"
-              name="name"
-              required
-            />
-          </div>
-
-          <div>
-            <label for="password">
-              <b>Password</b>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              required
-            />
-          </div>
-
-          <div>
-            <button type="submit">Register</button>
-            <label>
-              <input type="checkbox" checked="checked" name="remember" />{" "}
-              Remember me
-            </label>
-          </div>
+      <h2>Registration Page</h2>
+      <form className="registration-form" onSubmit={register}>
+        <div>
+          <p>First Name</p>
+          <input
+            type="text"
+            placeholder="First Name"
+            autoFocus
+            className="text-input"
+          />
         </div>
+        <div>
+          <p>Last Name</p>
+          <input
+            type="text"
+            placeholder="Last Name"
+            autoFocus
+            className="text-input"
+          />
+        </div>
+        <div>
+          <p>E-mail</p>
+          <input
+            type="text"
+            placeholder="E-mail"
+            autoFocus
+            className="text-input"
+          />
+        </div>
+        <div>
+          <p>Password</p>
+          <input
+            type="password"
+            placeholder="Password"
+            autoFocus
+            className="text-input"
+          />
+        </div>
+        <button className="button"> Sign up!</button>
       </form>
     </div>
   );
 };
 
 export { Registration as default };
+
+// <form className="registration" method="POST" action="/">
+// <div class="container">
+//   <div className="first-name">
+//     <b>First Name</b>>
+//     <input
+//       type="text"
+//       placeholder="Enter First Name"
+//       name="name"
+//       required
+//     />
+//   </div>
+
+//   <div className="last-name">
+//     <b>Last Name</b>
+
+//     <input
+//       type="text"
+//       placeholder="Enter Last Name"
+//       name="name"
+//       required
+//     />
+//   </div>
+
+//   <div className="e-mail">
+//     <b>E-mail</b>>
+//     <input
+//       type="text"
+//       placeholder="Enter Last Name"
+//       name="name"
+//       required
+//     />
+//   </div>
+
+//   <div className="password">
+//     <b>Password</b>>
+//     <input
+//       type="password"
+//       placeholder="Enter Password"
+//       name="password"
+//       required
+//     />
+//   </div>
+
+//   <div>
+//     <button type="submit">Register</button>
+//     <input type="checkbox" checked="checked" name="remember" /> Remember
+//     me
+//   </div>
+// </div>
+// </form>
