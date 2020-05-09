0241a186dde410078653d677d37299d1cd3651aa
KC Kai <kirstenckai@gmail.com>
2020-05-08 21:49:40 -0400

    Create routes between pages

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Axios from "axios";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Header from "./components/Header";
 import Registration from "./components/Registration";
-
-import Button from "react-bootstrap/Button";
-
 import LandingPage from './components/LandingPage';
 import Search from './components/Search';
 import SavedRecipes from './components/SavedRecipes';
+import Inventory from './components/Inventory';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
 
 function App() {
   Axios({
@@ -23,11 +24,20 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <LandingPage />
-      <Search />
-      <SavedRecipes />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path='/' component={LandingPage} exact={true}/>
+          <Route path="/login" component={Login} />
+          <Route path="/registration" component={Registration} />
+          <Route path="/search" component={Search} />
+          <Route path="/saved" component={SavedRecipes} /> 
+          <Route path="/inventory" component={Inventory} />  
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- recipe-app/src/components/Header.js
+++ recipe-app/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is the Header</h1>
+     <div>
+       <h1><a href="/">RecipeApp</a></h1>
+      <NavLink to="/login">Log In</NavLink>
+      <NavLink to="/registration">Sign Up</NavLink>
     </div>
   );
 };
