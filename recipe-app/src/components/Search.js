b3ec94bc83e00b6b1342e75445859d57bfb43900
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-05-09 18:13:33 -0400

    search form was added

--- recipe-app/src/components/Search.js
+++ recipe-app/src/components/Search.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 export default function Search() {
-  return(
-
-    <h1>Search</h1>
-  )
+  const search = e => {
+    e.preventDefault();
+    console.log("search button is pressed")
+  }
+  return (
+    <>
+      <h2>Search</h2>
+      <form onSubmit={search}>
+        <div>
+          <p> Enter what you have in a fridge</p>{" "}
+          <input type="text" placeholder="your ingredients here" autoFocus />
+        </div>
+        <button className="button">Search</button>
+      </form>
+    </>
+  );
 }
