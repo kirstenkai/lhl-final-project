937c1b3ec15e4800361e7bb037fd08f02ca6ded4
KC Kai <kirstenckai@gmail.com>
2020-05-09 22:52:52 -0400

    Add like button

--- recipe-app/src/components/RecipeCard.js
+++ recipe-app/src/components/RecipeCard.js
@@ -7,6 +7,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
 
 const useStyles = makeStyles({
   root: {
@@ -43,7 +45,7 @@ export default function RecipeCard() {
           Share
         </Button>
         <Button size="small" color="primary">
-          Learn More
+          <FavoriteBorderIcon />
         </Button>
       </CardActions>
     </Card>
