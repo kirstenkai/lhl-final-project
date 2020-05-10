1d7a50c223486741fcc06578ea68c8641ff1e213
KC Kai <kirstenckai@gmail.com>
2020-05-09 20:32:35 -0400

    Add styling to components

--- recipe-app/src/components/Header.js
+++ recipe-app/src/components/Header.js
@@ -1,14 +1,58 @@
-import React from "react";
+// import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Header = () => {
+// const Header = () => {
+//   return (
+//      <div>
+//        <h1><a href="/">RecipeApp</a></h1>
+//       <NavLink to="/login">Log In</NavLink>
+//       <NavLink to="/registration">Sign Up</NavLink>
+//     </div>
+//   );
+// };
+
+// export { Header as default };
+
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
   return (
-     <div>
-       <h1><a href="/">RecipeApp</a></h1>
-      <NavLink to="/login">Log In</NavLink>
-      <NavLink to="/registration">Sign Up</NavLink>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <NavLink to="/" className={classes.title}>
+            RecipeApp
+          </NavLink>
+          <NavLink to="/login" color="inherit">Login</NavLink>
+          <NavLink to="/registration" color="inherit">Sign Up</NavLink>
+
+        </Toolbar>
+      </AppBar>
     </div>
   );
-};
-
-export { Header as default };
+}
--- recipe-app/src/components/Login.js
+++ recipe-app/src/components/Login.js
@@ -1,3 +1,41 @@
+// import React from "react";
+
+// export default function Login(props) {
+//   const login = (e) => {
+//     e.preventDefault();
+//     console.log("login pressed");
+//   };
+//   return (
+//     <div>
+//       <h2>Login please</h2>
+//       <form className="login-form" onSubmit={login}>
+//         <div>
+//           <p>Login</p>
+//           <input
+//             type="text"
+//             placeholder="Email"
+//             autoFocus
+//             className="text-input"
+//             required
+//           />
+//         </div>
+
+//         <div>
+//           <p>Password</p>
+//           <input
+//             type="password"
+//             placeholder="Password"
+//             autoFocus
+//             className="text-input"
+//             required
+//           />
+//         </div>
+//         <button className="button"> Login!</button>
+//       </form>
+//     </div>
+//   );
+// }
+
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -13,29 +51,16 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundImage: 'url(img/flame-uploading.png)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
+      '#232946',
+    backgroundSize: '450px',
     backgroundPosition: 'center',
   },
   paper: {
@@ -57,13 +82,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignInSide() {
+export default function Login() {
   const classes = useStyles();
 
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
@@ -121,11 +145,11 @@ export default function SignInSide() {
               </Grid>
             </Grid>
             <Box mt={5}>
-              <Copyright />
             </Box>
           </form>
         </div>
       </Grid>
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
     </Grid>
   );
 }
