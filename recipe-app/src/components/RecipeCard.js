1a94360aa794eff6cda5f8861b52ae2b192897c7
Jeffrey <jeffrey.liu90@gmail.com>
2020-05-12 00:31:44 -0400

    added share to facebook the source url

--- recipe-app/src/components/RecipeCard.js
+++ recipe-app/src/components/RecipeCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -11,6 +11,7 @@ import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import Axios from "axios";
 import { FacebookShareButton } from "react-share";
 import { FacebookIcon } from "react-share";
+import ReactToPrint from "react-to-print";
 
 import Modal from "react-modal";
 
@@ -32,7 +33,7 @@ export default function RecipeCard({ image, title, id }) {
   const [state, setState] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
 
-  const source = [];
+  const componentRef = useRef();
 
   Modal.setAppElement("#root");
 
@@ -101,6 +102,7 @@ export default function RecipeCard({ image, title, id }) {
                         <FacebookIcon size={32} round={true}></FacebookIcon>
                       }
                     />
+
                     <button onClick={closeModal}>close</button>
                   </div>
                 );
