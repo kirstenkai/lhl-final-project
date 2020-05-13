a81de052fd745a245a4bff2d0ef0f10bb8c0b18c
KC Kai <kirstenckai@gmail.com>
2020-05-13 18:48:29 -0400

    Merge with master

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -28,15 +28,16 @@ require("dotenv").config();
 const SPOONACULAR_API = process.env.REACT_APP_SPOONACULAR_API;
 
 function App() {
-  // Axios({
-  //   method: "GET",
-  //   url: "http://localhost:5000/",
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   }
-  // }).then(res => {
-  //   console.log(res.data.message);
-  // });
+
+  Axios({
+    method: "GET",
+    url: "http://localhost:5000/",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data.message);
+  });
 
   const { loading, isAuthenticated } = useAuth0();
 
@@ -52,8 +53,8 @@ function App() {
         </header>
         <Switch>
           <Route exact path="/">
-            {isAuthenticated ? <Redirect to="/search" /> : <LandingPage />}
-          </Route>
+            { isAuthenticated ?  <Redirect to="/search" /> : <LandingPage /> }
+          </Route> 
           {console.log(isAuthenticated)}
           <PrivateRoute path="/profile" component={Profile} />
           <Route path="/login" component={Login} />
