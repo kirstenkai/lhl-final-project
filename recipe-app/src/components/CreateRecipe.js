eb5bd43b4df73c8319879d2349065a261e9e7367
KC Kai <kirstenckai@gmail.com>
2020-05-13 23:05:23 -0400

    Finish CreateRecipe component

--- recipe-app/src/components/CreateRecipe.js
+++ recipe-app/src/components/CreateRecipe.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {TextField, Button} from '@material-ui/core';
 
+import UploadButton from './UploadButton';
 
-export default function CreateRecipe(){
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(2,0),
+      width: '75%',
+    },
+  },
+}));
+
+export default function CreateRecipe() {
+  const classes = useStyles();
+ 
+
   return (
-  <div>
-    <h1>Create Recipe</h1>
-  
-    <p>Create Recipe form will go here</p>
-  </div>
-  )
+    <Fragment>
+      <h1>Create Recipe</h1>
+    
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        
+        <TextField
+          id="standard-textarea"
+          label="Title"
+          multiline
+        />
+    
+      </div>
+      <div>
+      <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Ingredients"
+          multiline
+          rows={4}
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Instructions"
+          multiline
+          rows={4}
+          variant="outlined"
+        />
+      </div>
+      <Button variant="contained" color="primary">Submit</Button>
+    </form>
+    <UploadButton />
+    </Fragment>
+  );
 }
