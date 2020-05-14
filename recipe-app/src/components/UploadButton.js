d5f35252937629afac99d80306e85d90e8fc6985
KC Kai <kirstenckai@gmail.com>
2020-05-14 18:04:48 -0400

    Remove redundant button

--- recipe-app/src/components/UploadButton.js
+++ recipe-app/src/components/UploadButton.js
@@ -20,18 +20,6 @@ export default function UploadButtons() {
 
   return (
     <div className={classes.root}>
-      <input
-        accept="image/*"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" color="primary" component="span">
-          Upload
-        </Button>
-      </label>
       <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
       <label htmlFor="icon-button-file">
         <IconButton color="primary" aria-label="upload picture" component="span">
